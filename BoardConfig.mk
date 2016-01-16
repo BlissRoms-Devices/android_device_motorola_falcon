c7d1874811595c73196c4d2754cefe5be5bccc92
Vatsal <koradiavatsal@hotmail.com>
2016-01-16 16:10:07 -0500

    Bliss-ify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,3 +43,32 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # inherit from the proprietary version
 -include vendor/motorola/falcon/BoardConfigVendor.mk
+
+# Block_Build
+Bliss_Build_Block:=1
+
+# Blissful building options
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_GRAPHITE := true
+#BLISS_STRICT := true
+BLISS_KRAIT := true
+BLISS_PIPE := true
+FLOOP_NEST_OPTIMIZE := true
+ENABLE_GCCONLY := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+FAST_MATH := true
+ENABLE_MODULAR_O3 := true
+ENABLE_LTO := true
+LINK_TIME_OPTIMIZATIONS := true
+TARGET_USE_ION_COMPAT := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_TC_ROM := 4.9-linaro
+TARGET_TC_KERNEL := 4.9-linaro
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+TARGET_ENABLE_UKM := true
+BLISS_DEVELOPER := Vatsal
+
+#WITH_LZMA_OTA := true
+#SaberMod
